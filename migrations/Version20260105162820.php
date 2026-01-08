3b002ccbae18d0a48334e3ef53c1c9aeb6027d17
Kadem9 <kadem.garnier@outlook.fr>
2026-01-08 14:00:00 +0100

    Infrastructure : Migrations doctrine

---
+++ migrations/Version20260105162820.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260105162820 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE logos (id VARCHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, url VARCHAR(500) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tier_list_items (id INT AUTO_INCREMENT NOT NULL, tier_list_id VARCHAR(36) NOT NULL, logo_id VARCHAR(36) NOT NULL, tier_category VARCHAR(255) NOT NULL, INDEX IDX_929486BDB25FD8A1 (tier_list_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tier_lists (id VARCHAR(36) NOT NULL, user_id VARCHAR(36) NOT NULL, UNIQUE INDEX UNIQ_3CC8C72CA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id VARCHAR(36) NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tier_list_items ADD CONSTRAINT FK_929486BDB25FD8A1 FOREIGN KEY (tier_list_id) REFERENCES tier_lists (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE tier_lists ADD CONSTRAINT FK_3CC8C72CA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tier_list_items DROP FOREIGN KEY FK_929486BDB25FD8A1');
+        $this->addSql('ALTER TABLE tier_lists DROP FOREIGN KEY FK_3CC8C72CA76ED395');
+        $this->addSql('DROP TABLE logos');
+        $this->addSql('DROP TABLE tier_list_items');
+        $this->addSql('DROP TABLE tier_lists');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
